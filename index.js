566947103977f3bc503df37aabbf7461f3c895dc
Rishabh Raj <33702025+rshvraj@users.noreply.github.com>
2024-02-24 12:09:31 +0530

    databse connection made

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
-
+app.use(express.json());
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
